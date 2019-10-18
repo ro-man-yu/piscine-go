1715c308c87ec54c8b777b08eb2f33f77b993011
ro-man-yu <ro-man-yu@github.com>
2019-10-19 01:42:21 +0600

    ulti

--- ultimatepointone.go
+++ ultimatepointone.go
@@ -1,16 +1,6 @@
 package ultimatepointone
 
-import "fmt"
-
 func UltimatePointOne(n ***int) {
+
 	***n = 1
-}
-
-func main() {
-
-	a := 0
-	b := &a
-	n := &b
-	UltimatePointOne(&n)
-	fmt.Println(a)
 }
