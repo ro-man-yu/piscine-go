723a8b6d1e1ffc6d287cfe076d82d081a30c5700
ro-man-yu <ro-man-yu@github.com>
2019-10-23 01:19:44 +0600

    recursi

---
+++ recursivefactorial.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func RecursiveFactorial(nb int) int {
+	if nb == 1 { 
+		return 1
+	} else if nb <= 0 || nb >= 20 {
+		return 0
+	}
+	return nb * RecursiveFactorial(nb - 1)	
+}
+	
+func main() {
+	arg := 4
+	fmt.Println(RecursiveFactorial(arg))
+}
