7bd32c06837ff7a1bc70dc2eb5ac3a2f6e539060
ro-man-yu <ro-man-yu@github.com>
2019-10-23 02:34:29 +0600

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,21 @@
-package piscine
+package main
+
+import "fmt"
 
 func RecursiveFactorial(nb int) int {
-
 	if nb <= 0 && nb >= 20 {
 		return 0
-	} else if nb == 1 {
+	}
+	if nb == 1 {
 		return 1
 	}
-	return nb * RecursiveFactorial(nb - 1)
+	if nb > 1 {
+		return nb * RecursiveFactorial(nb-1)
+	}	
+	return 0
+}
+
+func main() {
+	arg := 4
+	fmt.Println(RecursiveFactorial(arg))
 }
