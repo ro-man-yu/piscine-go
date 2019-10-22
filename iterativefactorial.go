c8f8feccd69e602a1a778f9147b15a5a4ad871b1
ro-man-yu <54017787+ro-man-yu@users.noreply.github.com>
2019-10-22 22:30:41 +0600

    Update iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,10 @@
 package piscine
 
-func IterativeFactorial(nb int) int 
-
-0 <= nb <= 20
-
-{
+func IterativeFactorial(nb int) int {
+	0 <= nb <= 20
 	a := 1 //var result int
 
-	for i := 1; i <= nb; i++ {
+	for i := 1; i < nb; i++ {
 		a = a * i
 	}
 	return a
