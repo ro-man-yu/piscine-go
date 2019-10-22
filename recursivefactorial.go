e6c807431fa4d04dc148295e9bdace70d3a632d6
ro-man-yu <ro-man-yu@github.com>
2019-10-23 04:25:40 +0600

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,19 +3,16 @@ package main
 import "fmt"
 
 func RecursiveFactorial(nb int) int {
-	if nb <= 0 && nb >= 20 {
+	if nb > 1 && nb < 20 {
+		return RecursiveFactorial(nb-1) * nb
+	} else if nb == 0 || nb == 1 {
+		return 1
+	} else {
 		return 0
 	}
-	if nb == 1 {
-		return 1
-	}
-	if nb > 1 {
-		return nb * RecursiveFactorial(nb-1)
-	}	
-	return 0
 }
 
 func main() {
-	arg := 4
+	arg := 19
 	fmt.Println(RecursiveFactorial(arg))
 }
