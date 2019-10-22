404503dc0ec307aa718e5f72a73eb0793787b262
ro-man-yu <ro-man-yu@github.com>
2019-10-22 23:28:28 +0600

    return 0

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,20 @@
-package piscine
+package main
+
+import "fmt"
 
 func IterativeFactorial(nb int) int {
 	a := 1 //var result int
 	if nb < 0 || nb > 20 {
 		return 0
-	} else {
+	}else {
 		for i := 1; i <= nb; i++ {
 			a = a * i
 		}
 		return a		
 	}	
+}
+ 
+func main() {
+	arg := 4
+	fmt.Println(IterativeFactorial(arg))
 }
